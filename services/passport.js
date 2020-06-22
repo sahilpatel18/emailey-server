2609e4b36f4cc947dea996a3bda88fc07afdc8cc
sahilpatel18 <sahil.41801@gmail.com>
2020-06-22 15:57:40 -0400

    tweak googleStrat setting again

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://localhost:5000/auth/google/callback",
       proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
