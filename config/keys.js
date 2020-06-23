d5f9d75c4fa5366c391346a4c3162f8c030d722a
sahilpatel18 <sahil.41801@gmail.com>
2020-06-23 11:17:10 -0400

    fixing

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-const e = require("express");
+const express = require("express");
 
 if(process.env.NODE_ENV === 'production'){
 //return prod keys
@@ -9,9 +9,3 @@ module.exports = require('./dev')
 }
 
 
-
-
-// prod 
-// mongodb+srv://myuser:TEI3hCLPUX9pJIZC@cluster0-t4bqb.mongodb.net/emaily-prod?retryWrites=true&w=majority
-// myuser
-// TEI3hCLPUX9pJIZC
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,17 +10,13 @@ module.exports = (app) => {
 
   app.get("/auth/google/callback", passport.authenticate("google"));
 
-  app.get('/api/logout', (req,res) => {
+  app.get("/api/logout", (req, res) => {
     req.logout();
-    console.log('logged out');
-    res.send(req.user)
-  })
+    console.log("logged out");
+    res.send(req.user);
+  });
 
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
- 
-    
-
   });
 };
-
