f47482ff6beacba9c74d1eeeca52efe6c0426fc1
sahilpatel18 <sahil.41801@gmail.com>
2020-06-24 16:40:23 -0400

    login vs logout UI along with routing

--- clientt/src/components/App.js
+++ clientt/src/components/App.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+import Landing from '../components/Landing'
 
-const App = () => {
-  return (
-    <div className='container'>
-      <BrowserRouter>
-        <div>
-          <Header />
-          {/* <Route exact path='/' component={Landing} />
-                <Route exact path='/surveys' component={Landing} />
-                <Route path='/surveys/new' component={Landing} />
-                <Route path='/' component={Landing} />  */}
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+const Dashboard = () => {
+  return <div>Dashboard</div>;
 };
 
-export default App;
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <BrowserRouter>
+          <Header />
+          <div>
+            <Route exact path='/' component={Landing} /> 
+            <Route exact path='/surveys' component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
