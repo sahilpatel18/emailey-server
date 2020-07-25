d4f7269e9939c6bc7c596ead9719941d44700e44
sahilpatel18 <sahil.41801@gmail.com>
2020-07-25 16:44:39 -0400

    changed setting in proxy to work on local

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,6 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
+  console.log(app);
   app.use(
     ["/api", "/auth/google"],
     createProxyMiddleware({
